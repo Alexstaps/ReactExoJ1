1649108fc7533eb1b7f4f315cd591ddd782b2de8
Alexandre Houcine <alexandrehoucine@mbpdealexandre.lan>
2023-10-31 10:58:42 +0100

    Creation componant Jobs

--- src/assets/components/Job.jsx
+++ src/assets/components/Job.jsx
@@ -1,8 +1,13 @@
 const Job = (props) => {
-    return <div className={props.className}>
+    return (
+        <div className={props.className}>
         <div><h2>{props.title}</h2></div>
         <div><p><span>{props.contractType}</span> - <span>{props.country}</span> - <span>{props.city}</span></p></div>
+        </div>
 
-    </div>
+    )
+        
+
+
 }
 export default Job;
---
+++ src/assets/components/Jobs.jsx
@@ -0,0 +1,53 @@
+import Job from "./Job";
+const Jobs = ()=>{
+return (
+    <main className="container">
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+        <Job 
+        title="Full Time Sales Associate - Sydney Boutique"
+        contractType="CDI"
+        country="Australie"
+        city="Sydney"/>
+    </main>
+)
+}
+export default Jobs;
